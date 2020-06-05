1d9be16a70d010ddf6118b1119d11a29ec0fb869
ychakir <yassine.chakir@ilemgroup.com>
2020-06-05 21:04:43 +0100

    Use core BaseLoggingAspect. #7

--- src/main/java/ma/tiwtiw/menu/aspect/LoggingAspect.java
+++ src/main/java/ma/tiwtiw/menu/aspect/LoggingAspect.java
@@ -1,105 +1,13 @@
 package ma.tiwtiw.menu.aspect;
 
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
+import ma.tiwtiw.core.controller.aspect.BaseLoggingAspect;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-/**
- * Aspect for logging execution of service and repository Spring components.
- */
 @Slf4j
 @Aspect
 @Component
-public class LoggingAspect {
+public class LoggingAspect extends BaseLoggingAspect {
 
-  /**
-   * Pointcut for controllers.
-   */
-  @Pointcut("within(ma.tiwtiw.core.controller.BaseController+)")
-  public void controllerPointcut() {
-  }
-
-  /**
-   * Pointcut for services.
-   */
-  @Pointcut("within(ma.tiwtiw.core.service.BaseService+)")
-  public void servicePointcut() {
-  }
-
-  /**
-   * Pointcut for mongo repository.
-   */
-  @Pointcut("within(org.springframework.data.mongodb.repository.MongoRepository+)")
-  public void repositoryPointcut() {
-  }
-
-  /**
-   * Advice that logs methods throwing exceptions.
-   *
-   * @param joinPoint join point for advice
-   * @param e exception
-   */
-  @AfterThrowing(pointcut = "controllerPointcut() || servicePointcut()", throwing = "e")
-  public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-    log.error("Exception in {}.{}() with cause = {}",
-        joinPoint.getTarget().getClass().getName(),
-        joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
-  }
-
-  /**
-   * Advice that logs when a method is entered and exited
-   *
-   * @param joinPoint join point for advice
-   * @return result
-   * @throws Throwable throws IllegalArgumentException
-   */
-  @Around("controllerPointcut() || servicePointcut()")
-  public Object logAroundControllerOrService(ProceedingJoinPoint joinPoint) throws Throwable {
-    return logAround(joinPoint, joinPoint.getTarget().getClass().getName());
-  }
-
-  /**
-   * Advice that logs when a method is entered and exited
-   *
-   * @param joinPoint join point for advice
-   * @return result
-   * @throws Throwable throws IllegalArgumentException
-   */
-  @Around("repositoryPointcut()")
-  public Object logAroundRepository(ProceedingJoinPoint joinPoint) throws Throwable {
-    return logAround(joinPoint, joinPoint.getSignature().getDeclaringTypeName());
-  }
-
-  /**
-   * Advice that logs when a method is entered and exited.
-   *
-   * @param joinPoint join point for advice
-   * @param className class name
-   * @return result
-   * @throws Throwable throws IllegalArgumentException
-   */
-  private Object logAround(ProceedingJoinPoint joinPoint, String className) throws Throwable {
-    if (log.isDebugEnabled()) {
-      log.debug("Enter: {}.{}() with argument[s] = {}", className,
-          joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
-    }
-    try {
-      Object result = joinPoint.proceed();
-      if (log.isDebugEnabled()) {
-        log.debug("Exit: {}.{}() with result = {}", className, joinPoint.getSignature().getName(),
-            result);
-      }
-      return result;
-    } catch (IllegalArgumentException e) {
-      log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()), className,
-          joinPoint.getSignature().getName());
-      throw e;
-    }
-  }
 }
